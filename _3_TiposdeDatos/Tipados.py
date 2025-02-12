915bfcac5bc23a89bea345e1a529b2a944b1e8d9
Karla Angelica Rojano <l23240010@smartin.tecnm.mx>
2025-02-12 16:34:07 -0600

    Tipos de datos

---
+++ _3_TiposdeDatos/Tipados.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    edad:int = 1234567890987654321123456789009876543212346
+    nombre: str ="Maria"
+    altura: float = 1.1234567890123456789
+    activo: bool= True
+
+    print(edad)
+    print(nombre)
+    print(altura)
+    print(activo)
