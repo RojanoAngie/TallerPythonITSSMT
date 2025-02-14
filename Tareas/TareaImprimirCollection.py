5bc9e2e8b2b07006b50d3cf83df2c397bc203a2e
MSC Tomás González <tomas.gonzalez@smartin.tecnm.mx>
2025-02-14 05:55:11 -0600

    Indicaciones Tarea 1

--- Tareas/TareaImprimirCollection.py
+++ Tareas/TareaImprimirCollection.py
@@ -1,2 +1,9 @@
-# utiliza un for para imprimir escribir dato por dato de las colecciones
- #fecha de entrega lunes 17 de febrero
+#Tarea 12 de Febrero del 2025
+"""
+   Una vez que conociste los tipos de datos de la categoría Collections de Python
+   Ahora la tarea consiste en declarar una variabla para cada elemento, ingresa de forma manual, al menos 30 valores para cada elemento,
+   y finalmente utiliza un for para imprimir cada uno de sus elementos en pantalla.
+   Fecha límite de entrega: Lúunes 17 de febrero del 2025
+
+   ¡Éxito en esta actividad!
+"""
